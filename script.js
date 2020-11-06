6ef0e09ef8a7a4758ee6892c1ea89b82844675e5
LisaAAAlisa <alisaamacedo@gmail.com>
2020-11-05 21:54:55 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+// Assignment Code
+var generateBtn = document.querySelector
+("#generate");
+
+function generatePassword {
+
+var passChars = "0123456789@#$" +
+            "abcdefghijklmnopqrstuvwxyz" +
+            "ABCDEFGHIJKLMNOPRSTUVWXYZ";
+
+}
+
+var passLength= prompt("Please choose password length between 8 and 128 characters");
+var lowerCase= prompt("Do you want your password to contain lowercase letters?");
+var upperCase= prompt("Do you want your password to contain uppercase letters?");
+var numbers= prompt("Do you want your password to contain numbers?");
+var special= prompt("Do you want your password to contain special characters?");
+
+if (passLength < 8 || passLength > 128) {
+  alert("the password requirement is not met");
+  return;
+}
+if(!(lowerCase || upperCase || numbers || special)) {
+  alert("Password must contain at least one type of character.");
+  return;
+}
+var password = "";
+
+if (lowerCase) {
+  
+}
+if (upperCase === true) {
+
+}
+if (numbers === true) {
+
+}
+if (special=== true) {
+
+}
+
+var minimumCount = 0;
+
+for (var i=0; i=<128; i++) {
+  var oneCh = Math.floor(Math.random() *specialChars,length +1);
+  password += specialChars.charAt(oneCh)
+  return Math.random(password);
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+function generatePassword(); {
+
+generateBtn.addEventListener("click", writePassword);
