c520ce1faf2a496294082eca2ff6dd1ed6d98f59
LisaAAAlisa <alisaamacedo@gmail.com>
2020-11-08 18:52:49 -0600

    finished assignment, aay!

--- script.js
+++ script.js
@@ -1,51 +1,73 @@
 // Assignment Code
 var generateBtn = document.querySelector
-("#generate");
+  ("#generate");
 
-function generatePassword {
+function generatePassword() {
 
-var passChars = "0123456789@#$" +
-            "abcdefghijklmnopqrstuvwxyz" +
-            "ABCDEFGHIJKLMNOPRSTUVWXYZ";
+  var numbers = "0123456789";
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var special = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
-}
+  // get user input
+  var passLength = prompt("Please choose password length between 8 and 128 characters");
+  //validate user input
+  if (passLength < 8 || passLength > 128) {
+    alert("please choose password between 8 to 128 characters");
+    return;
 
-var passLength= prompt("Please choose password length between 8 and 128 characters");
-var lowerCase= prompt("Do you want your password to contain lowercase letters?");
-var upperCase= prompt("Do you want your password to contain uppercase letters?");
-var numbers= prompt("Do you want your password to contain numbers?");
-var special= prompt("Do you want your password to contain special characters?");
+  }
+  var hasLowerCase = confirm("Do you want your password to contain lowercase letters?");
+  var hasUpperCase = confirm("Do you want your password to contain uppercase letters?");
+  var hasNumbers = confirm("Do you want your password to contain numbers?");
+  var hasSpecial = confirm("Do you want your password to contain special characters?");
 
-if (passLength < 8 || passLength > 128) {
-  alert("the password requirement is not met");
-  return;
-}
-if(!(lowerCase || upperCase || numbers || special)) {
-  alert("Password must contain at least one type of character.");
-  return;
-}
-var password = "";
-var charArray = [0];
-
-if (lowerCase) {
-
-}
-if (upperCase) {
-
-}
-if (numbers) {
-
-}
-if (special) {
-
-}
-
-var minimumCount = 0;
-
-for (var i=0; i=<128; i++) {
-  var oneCh = Math.floor(Math.random() *specialChars,length +1);
-  password += specialChars.charAt(oneCh)
-  return Math.random(password);
+  // making sure that at least one of the variables is represented
+  if (!(hasLowerCase || hasUpperCase || hasNumbers || hasSpecial)) {
+    alert("you must choose at least one of the criteria");
+    return;
+  }
+  var password = [];
+  // setting the loop to run until the password length requirement is satisfied
+  for (i = 0; i <= passLength; i++) {
+    if (hasSpecial) {
+      password.push(special[Math.floor(Math.random() * special.length)]);
+      if (password.length == passLength) {
+        break;
+      }
+      console.log(password);
+    }
+    if (hasNumbers) {
+      password.push(numbers[Math.floor(Math.random() * numbers.length)]);
+      if (password.length == passLength) {
+        break;
+      }
+      console.log(password);
+    }
+    if (hasUpperCase) {
+      password.push(upperCase[Math.floor(Math.random() * upperCase.length)]);
+      if (password.length == passLength) {
+        break;
+      }
+      console.log(password);
+    }
+    if (hasLowerCase) {
+      password.push(lowerCase[Math.floor(Math.random() * lowerCase.length)]);
+      if (password.length == passLength) {
+        break;
+      }
+    }
+  }
+  console.log(password);
+// shuffle password result and join the string to remove commas
+  var charCount = password.length;
+  var shufflePass = [];
+  for (i = 0; i < charCount; i++) {
+    var index = Math.floor(Math.random() * password.length);
+    shufflePass.push(password[index]);
+    password.splice(index, 1);
+  }
+  return (shufflePass.join(''));
 }
 
 // Write password to the #password input
